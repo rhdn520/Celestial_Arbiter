1b0eadb9b01c049e06d3d85e36eff7d6b7ba7941
seungwoo <520gow@gmail.com>
2023-11-25 23:54:37 +0900

    feat: added logics to update variables in SceneManager

--- scenemanager.js
+++ scenemanager.js
@@ -1,7 +1,8 @@
 class SceneManager{
     constructor(){
         this.chatLog = [];
-        this.conversationStatus = 'before'; //'before', 'during', 'after' 3가지. Load Scene과 연계
+        this.conversationStatus = 'before'; //'before', 'during', 'after' 3가지. Load Scene과 연계된 변수
+        this.nextLifeImg = null;
     }
 
     loadScene(){
@@ -21,20 +22,38 @@ class SceneManager{
     }
 
     loadBeforeScene(){
-
+        text('This is BEFORE Scene');
     }
 
     loadDuringScene(){
-
+        text('This is DURING Scene');
     }
 
     loadAfterScene(){
-
+        text('This is AFTER Scene');
     }
 
-    changeScene(){
-
+    changeScene(newConvStatus){
+        this.conversationStatus = newConvStatus;
+        console.log('Conversation Status Changed');
     }
+
+    updateChatLog(newChat){
+        this.chatLog.push(newChat);
+        console.log('Chatting Log Updated');
+    }
+
+    updateNextLifeImg(nextLifeImg){
+        this.nextLifeImg = nextLifeImg;
+        console.log('Next Life Image Updated');
+    }
+
+    resetVariables(){
+        this.chatLog = [];
+        this.conversationStatus = 'before';
+        this.nextLifeImg = null;
+    }
+
 
 
 
