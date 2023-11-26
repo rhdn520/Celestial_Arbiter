b82ef7b71c0f2c51d3bef4428feb668d2debbcc1
seungwoo <520gow@gmail.com>
2023-11-26 11:37:40 +0900

    added logic to fetch mp3 from voice api

--- class/ttshandler.js
+++ class/ttshandler.js
@@ -1,11 +1,34 @@
+
 class TTSHandler {
     constructor() {
-        this.clientID = "ka7gfav4ae";
-        this.secretID = "k3p6qMRm8cK2jzJHveKyrndyzMQuZZAW9g7PiT78";
-        this.options = {
-            url: "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts",
-            form: { speaker: 'nara', volume: '0', speed: '0', pitch: '0', text: '좋은 하루 되세요', format: 'mp3' },
-            headers: { 'X-NCP-APIGW-API-KEY-ID': this.clientID, 'X-NCP-APIGW-API-KEY': this.secretID },
+    }
+
+    async fetchTTS(userMessage="둥땅둥땅 걸어가는 펭귄") {
+        
+        let clientID = "ka7gfav4ae";
+        let secretID = "k3p6qMRm8cK2jzJHveKyrndyzMQuZZAW9g7PiT78";
+        let url = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts";  
+        let encText = encodeURIComponent(userMessage);
+        let data = `speaker=nsabina&volume=0&speed=0&pitch=0&format=mp3&text=${encText}`
+        let options = {
+            method: "POST",
+            body: data,
+            headers: {
+                'X-NCP-APIGW-API-KEY-ID': clientID,
+                'X-NCP-APIGW-API-KEY': secretID,
+                "Content-Type": "application/x-www-form-urlencoded",
+                "charset": "utf-8"
+            },
         };
+
+        try {
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                throw new Error(`Error Code: ${response.status}`);
+            }
+            return await response.blob();
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
 let scene;
+let tts;
+let ttsSound;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   scene = new SceneManager();
+  tts = new TTSHandler();
+  // testTTS();
 }
 
 function draw() {
-  // background(220);
-  // scene.loadScene();
 }
+
+
+//test code for TTS
+function testTTS(){ 
+  tts.fetchTTS("아, 아, 마이크테스트!").then(mp3URL => {
+    loadSound(mp3URL,(sound)=>{
+      sound.play()
+    });
+  })
+}
+
