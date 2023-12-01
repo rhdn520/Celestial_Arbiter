508aa62d743685280bb21786fa581f8cb1de9cb0
seungwoo <520gow@gmail.com>
2023-12-01 16:01:57 +0900

    file: moved sound class to sound.js

--- sketch.js
+++ sketch.js
@@ -1,29 +1,5 @@
 let sounds = [];
 
-class Sound {
-  constructor(soundFile) {
-    this.sound = loadSound(soundFile, this.loaded.bind(this));
-    this.isPlaying = false;
-  }
-
-  loaded() {
-    this.duration = this.sound.duration();
-  }
-
-  play() {
-    if (!this.isPlaying) {
-      this.sound.play();
-      this.isPlaying = true;
-      setTimeout(() => {
-        this.isPlaying = false;
-      }, this.duration * 1000);
-    }
-  }
-
-  isPlayingNow() {
-    return this.isPlaying;
-  }
-}
 
 function preload() {
   // MP3 파일들을 미리 로드
