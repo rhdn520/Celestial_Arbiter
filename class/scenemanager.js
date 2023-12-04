f0a6c133ba4e2f38578dd25658eee2309e1a3eef
seungwoo <520gow@gmail.com>
2023-12-04 23:07:40 +0900

    added seungwoo's api key

--- class/scenemanager.js
+++ class/scenemanager.js
@@ -59,10 +59,10 @@ class SceneManager {
         receiptDummyImg.resize(width,receiptDummyImg.height*(width/receiptDummyImg.width));
         if(this.afterSceneLoadMillis === null){
             this.afterSceneLoadMillis = millis();
-            text(`Press ESC to restart (${10}s)`, width/2, height - 60)
+            text(`Press ESC to restart (${30}s)`, width/2, height/10)
         }else{
             let countdown = int(31 + (this.afterSceneLoadMillis - millis())/1000)
-            text(`Press ESC to restart (${countdown}s)`, width/2, height - 60)
+            text(`Press ESC to restart (${countdown}s)`, width/2, height/10)
             if(countdown === 0){
                 this.afterSceneLoadMillis = null;
                 this.globalVar.conversationStatus = 'before';
