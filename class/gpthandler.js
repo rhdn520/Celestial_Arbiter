267283b82f0b19aee51a8a7a6b2136b3df11b90a
seungwoo <520gow@gmail.com>
2023-12-06 10:18:53 +0900

    changed gpt model & api key

--- class/gpthandler.js
+++ class/gpthandler.js
@@ -1,7 +1,7 @@
 class GPTHandler {
   constructor(_globalVar) {
-    // this.apiKey = "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";
-    this.apiKey = "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"; //승우
+    this.apiKey = "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";
+    // this.apiKey = "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"; //승우
     this.apiUrl = "https://api.openai.com/v1/chat/completions";
     this.prompt = promptText;
     this.globalVar = _globalVar;
@@ -28,7 +28,7 @@ class GPTHandler {
           Authorization: `Bearer ${this.apiKey}`,
         },
         body: JSON.stringify({
-          model: "gpt-4-1106-preview",
+          model: "gpt-3.5-turbo",
           messages: [
             { role: "system", content: this.prompt }, //프롬프트 넣는 곳
             ...chatLog,
