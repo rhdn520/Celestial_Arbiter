7fc338740cb7c05c9a7a82a82ecd9db161403235
seungwoo <520gow@gmail.com>
2023-12-06 15:06:56 +0900

    changed prompt to answer only in english

--- class/promptText.js
+++ class/promptText.js
@@ -58,6 +58,7 @@ const receiptPromptText = `
 -Get 
 -Focus on soul's life. Pick five important values that the soul pursued. Don't add additional description.
 -Score the importance of each values out of 10.
+-Answer in english.
 `
 
 
