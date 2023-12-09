8ea8dd0975430457a0075aaf4f7570c2d6cfc10e
seungwoo <520gow@gmail.com>
2023-12-09 11:14:14 +0900

    changed hierarchy of divs and added classes

--- class/receipt.js
+++ class/receipt.js
@@ -32,10 +32,17 @@ class Receipt {
     rectMode(CORNER);
     rect(this.receiptX, this.receiptY, this.width, this.height);
 
+    //Container
+    let receiptContainer = createDiv();
+    receiptContainer.position(this.xPos, this.yPos);
+    receiptContainer.size(this.divWidth);
+    receiptContainer.addClass('receipt-container');
+
     //Div1
     let div1 = createDiv("LIFE RECEIPT");
-    div1.position(this.xPos, this.yPos);
-    div1.size(this.divWidth, this.div1Height);
+    receiptContainer.child(div1);
+    div1.style("height", "fit-content");
+    div1.style("padding", "20px 0px")
     div1.style("background-color", "rgba(255,255,255,0.8)");
     div1.style("text-align", "center");
     div1.style("font-family", "typewr_b");
@@ -47,9 +54,11 @@ class Receipt {
 
     //Div2
     let div2 = createDiv(this.globalVar.receiptData.judge_summary);
-    div2.position(this.xPos, this.yPos + this.div1Height);
-    div2.size(this.divWidth, this.div2Height);
-    div2.addClass('judge_summary');
+    receiptContainer.child(div2);
+    // div2.position(this.xPos, this.yPos + this.div1Height);
+    // div2.size(this.divWidth, this.div2Height);
+    div2.addClass('judge-summary');
+
     //Div3
 
     let div3Text = "";
@@ -60,8 +69,10 @@ class Receipt {
     div3Text += `Value5<br>${this.globalVar.receiptData.value5}<br>`;
 
     let div3 = createDiv(div3Text);
-    div3.position(this.xPos, this.yPos + this.div1Height + this.div2Height);
-    div3.size(this.divWidth, this.div2Height);
+    // div3.position(this.xPos, this.yPos + this.div1Height + this.div2Height);
+    // div3.size(this.divWidth, this.div2Height);
+    div3.style("height", "fit-content");
+    div3.style("padding", "25px")
     div3.style("background-color", "rgba(255,255,255,0.8)");
     div3.style("text-align", "center");
     div3.style("font-family", "typewr_b");
@@ -69,102 +80,110 @@ class Receipt {
     div3.style("display", "flex");
     div3.style("justify-content", "center");
     div3.style("align-items", "center");
-    // div3.style("border", "1px dotted#000");
+    receiptContainer.child(div3);
+
+    let receiptBottom = createDiv();
+    receiptBottom.addClass('receipt-bottom');
+    
 
     //Div4
     let div4 = createDiv(
       "Life Receipt\nINTRODUCTION TO INFORMATION-CULTURE TECHNOLOGY"
     );
-    div4.position(
-      this.xPos,
-      this.yPos + this.div1Height + this.div2Height + this.div3Height
-    );
-    div4.size(this.div4Width, this.div4Height);
-    div4.style("background-color", "rgba(255,255,255,0.8)");
-    div4.style("text-align", "center");
-    div4.style("font-family", "typewr_b");
-    div4.style("font-size", "7px");
-    div4.style("display", "flex");
-    div4.style("justify-content", "center");
-    div4.style("align-items", "center");
-    div4.style("border", "1px dotted#000");
+    // div4.position(
+    //   this.xPos,
+    //   this.yPos + this.div1Height + this.div2Height + this.div3Height
+    // );
+    // div4.size(this.div4Width, this.div4Height);
+    // div4.style("background-color", "rgba(255,255,255,0.8)");
+    // div4.style("text-align", "center");
+    // div4.style("font-family", "typewr_b");
+    // div4.style("font-size", "7px");
+    // div4.style("display", "flex");
+    // div4.style("justify-content", "center");
+    // div4.style("align-items", "center");
+    // div4.style("border", "1px dotted#000");
+    div4.addClass('receipt-bottom-element');
 
     //Div5
     let div5 = createDiv("2023-2\nTEAM F");
-    div5.position(
-      this.xPos + this.div4Width,
-      this.yPos + this.div1Height + this.div2Height + this.div3Height
-    );
-    div5.size(this.div4Width, this.div5Height);
-    div5.style("background-color", "rgba(255,255,255,0.8)");
-    div5.style("text-align", "center");
-    div5.style("font-family", "typewr_b");
-    div5.style("font-size", "7px");
-    div5.style("display", "flex");
-    div5.style("justify-content", "center");
-    div5.style("align-items", "center");
-    div5.style("border", "1px dotted#000");
+    div5.addClass('receipt-bottom-element');
+    // div5.position(
+    //   this.xPos + this.div4Width,
+    //   this.yPos + this.div1Height + this.div2Height + this.div3Height
+    // );
+    // div5.size(this.div4Width, this.div5Height);
+    // div5.style("background-color", "rgba(255,255,255,0.8)");
+    // div5.style("text-align", "center");
+    // div5.style("font-family", "typewr_b");
+    // div5.style("font-size", "7px");
+    // div5.style("display", "flex");
+    // div5.style("justify-content", "center");
+    // div5.style("align-items", "center");
+    // div5.style("border", "1px dotted#000");
 
     //Div6
     let div6 = createDiv("SCAN THIS QR CODE");
-    div6.position(
-      this.xPos + this.div4Width * 2,
-      this.yPos + this.div1Height + this.div2Height + this.div3Height
-    );
-    div6.size(this.div4Width, this.div6Height);
-    div6.style("background-color", "rgba(255,255,255,0.8)");
-    div6.style("text-align", "center");
-    div6.style("font-family", "typewr_b");
-    div6.style("font-size", "7px");
-    div6.style("display", "flex");
-    div6.style("justify-content", "center");
-    div6.style("align-items", "center");
-    div6.style("border", "1px dotted#000");
+    div6.addClass('receipt-bottom-element');
+    // div6.style("background-color", "rgba(255,255,255,0.8)");
+    // div6.style("text-align", "center");
+    // div6.style("font-family", "typewr_b");
+    // div6.style("font-size", "7px");
+    // div6.style("display", "flex");
+    // div6.style("justify-content", "center");
+    // div6.style("align-items", "center");
+    // div6.style("border", "1px dotted#000");
 
     //Div7
     let div7 = createDiv('20231219');
-    div7.position(
-      this.xPos,
-      this.yPos + this.div1Height + this.div2Height + this.div3Height + this.div4Height
-    );
-    div7.size(this.div4Width, this.div7Height);
-    div7.style("background-color", "rgba(255,255,255,0.8)");
-    div7.style("text-align", "center");
-    div7.style("font-family", "barcord");
+    div7.addClass('receipt-bottom-element');
+    div7.style("font-family", "barcord");  
     div7.style("font-size", "36px");
-    div7.style("display", "flex");
-    div7.style("justify-content", "center");
-    div7.style("align-items", "center");
-    div7.style("border", "1px none#000");
+    // div7.style("background-color", "rgba(255,255,255,0.8)");
+    // div7.style("text-align", "center");
+    // div7.style("display", "flex");
+    // div7.style("justify-content", "center");
+    // div7.style("align-items", "center");
+    // div7.style("border", "1px none#000");
 
     //Div8
     let div8 = createDiv('');
-    div8.position(
-      this.xPos + this.div4Width,
-      this.yPos + this.div1Height + this.div2Height + this.div3Height + this.div4Height
-    );
-    div8.size(this.div4Width, this.div8Height);
-    div8.style("background-color", "rgba(255,255,255,0.8)");
-    div8.style("display", "flex");
-    div8.style("justify-content", "center");
-    div8.style("align-items", "center");
-    div8.style("border", "1px none#000");
+    div8.addClass('receipt-bottom-element');
+    // div8.position(
+    //   this.xPos + this.div4Width,
+    //   this.yPos + this.div1Height + this.div2Height + this.div3Height + this.div4Height
+    // );
+    // div8.size(this.div4Width, this.div8Height);
+    // div8.style("background-color", "rgba(255,255,255,0.8)");
+    // div8.style("display", "flex");
+    // div8.style("justify-content", "center");
+    // div8.style("align-items", "center");
+    // div8.style("border", "1px none#000");
 
     //let imgElement;
     //imgElement=createImg('')
 
     //Div9
     let div9 = createDiv('');
-    div9.position(
-      this.xPos + this.div4Width * 2,
-      this.yPos + this.div1Height + this.div2Height + this.div3Height + this.div4Height
-    );
-    div9.size(this.div4Width, this.div9Height);
-    div9.style("background-color", "rgba(255,255,255,0.8)");
-    div9.style("display", "flex");
-    div9.style("justify-content", "center");
-    div9.style("align-items", "center");
-    div9.style("border", "1px none#000");
+    div9.addClass('receipt-bottom-element');
+    // div9.position(
+    //   this.xPos + this.div4Width * 2,
+    //   this.yPos + this.div1Height + this.div2Height + this.div3Height + this.div4Height
+    // );
+    // div9.size(this.div4Width, this.div9Height);
+    // div9.style("background-color", "rgba(255,255,255,0.8)");
+    // div9.style("display", "flex");
+    // div9.style("justify-content", "center");
+    // div9.style("align-items", "center");
+    // div9.style("border", "1px none#000");
 
+
+    receiptBottom.child(div4);
+    receiptBottom.child(div5);
+    receiptBottom.child(div6);
+    receiptBottom.child(div7);
+    receiptBottom.child(div8);
+    receiptBottom.child(div9);
+    receiptContainer.child(receiptBottom);
   }
 }
