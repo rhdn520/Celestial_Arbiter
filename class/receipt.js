b96d6ddf173492505e8225df0052cf1207856118
seungwoo <520gow@gmail.com>
2023-12-11 17:23:34 +0900

    added style to receipt

--- class/receipt.js
+++ class/receipt.js
@@ -48,7 +48,7 @@ class Receipt {
     div1.style("display", "flex");
     div1.style("justify-content", "center");
     div1.style("align-items", "center");
-    div1.style("border", "1px dotted#000");
+    div1.style("border-bottom", "1px dotted#000");
 
     //Div2
     let div2 = createDiv(this.globalVar.receiptData.judge_summary);
@@ -59,122 +59,61 @@ class Receipt {
 
     //Div3
 
-    let div3Text = "";
-    div3Text += `Value1<br>${this.globalVar.receiptData.value1}<br>`;
-    div3Text += `Value2<br>${this.globalVar.receiptData.value2}<br>`;
-    div3Text += `Value3<br>${this.globalVar.receiptData.value3}<br>`;
-    div3Text += `Value4<br>${this.globalVar.receiptData.value4}<br>`;
-    div3Text += `Value5<br>${this.globalVar.receiptData.value5}<br>`;
+    let div3Text = "<p>";
+    div3Text += `1 ${this.globalVar.receiptData.value1}<br>`;
+    div3Text += `2 ${this.globalVar.receiptData.value2}<br>`;
+    div3Text += `3 ${this.globalVar.receiptData.value3}<br>`;
+    div3Text += `4 ${this.globalVar.receiptData.value4}<br>`;
+    div3Text += `5 ${this.globalVar.receiptData.value5}<br></p>`;
 
     let div3 = createDiv(div3Text);
-    // div3.position(this.xPos, this.yPos + this.div1Height + this.div2Height);
-    // div3.size(this.divWidth, this.div2Height);
+    div3.style("width","100%");
     div3.style("height", "fit-content");
-    div3.style("padding", "25px")
-    div3.style("background-color", "rgba(255,255,255,0.8)");
-    div3.style("text-align", "center");
+    div3.style("padding", "0 20px");
+    div3.style("text-align", "left");
     div3.style("font-family", "myfontrunes");
-    div3.style("font-size", "11px");
+    div3.style("font-size", "20px");
     div3.style("display", "flex");
-    div3.style("justify-content", "center");
+    div3.style("flex-flow", "row nowrap");
+    div3.style("justify-content", "flex-start");
     div3.style("align-items", "center");
+    div3.style("border-bottom","1px dotted black");
     receiptContainer.child(div3);
 
     let receiptBottom = createDiv();
     receiptBottom.addClass('receipt-bottom');
     
-
     //Div4
     let div4 = createDiv(
-      "Life Receipt\nINTRODUCTION TO INFORMATION-CULTURE TECHNOLOGY"
+      "<span>Life Receipt\nINTRODUCTION TO INFORMATION-CULTURE TECHNOLOGY</span>"
     );
-    // div4.position(
-    //   this.xPos,
-    //   this.yPos + this.div1Height + this.div2Height + this.div3Height
-    // );
-    // div4.size(this.div4Width, this.div4Height);
-    // div4.style("background-color", "rgba(255,255,255,0.8)");
-    // div4.style("text-align", "center");
-    // div4.style("font-family", "typewr_b");
-    // div4.style("font-size", "7px");
-    // div4.style("display", "flex");
-    // div4.style("justify-content", "center");
-    // div4.style("align-items", "center");
-    // div4.style("border", "1px dotted#000");
     div4.addClass('receipt-bottom-element');
 
     //Div5
-    let div5 = createDiv("2023-2\nTEAM F");
+    let div5 = createDiv("<span>2023-2\nTEAM F</span>");
     div5.addClass('receipt-bottom-element');
-    // div5.position(
-    //   this.xPos + this.div4Width,
-    //   this.yPos + this.div1Height + this.div2Height + this.div3Height
-    // );
-    // div5.size(this.div4Width, this.div5Height);
-    // div5.style("background-color", "rgba(255,255,255,0.8)");
-    // div5.style("text-align", "center");
-    // div5.style("font-family", "typewr_b");
-    // div5.style("font-size", "7px");
-    // div5.style("display", "flex");
-    // div5.style("justify-content", "center");
-    // div5.style("align-items", "center");
-    // div5.style("border", "1px dotted#000");
 
     //Div6
-    let div6 = createDiv("SCAN THIS QR CODE");
+    let div6 = createDiv("<span>SCAN THIS QR CODE<br><br>⬇</span>");
     div6.addClass('receipt-bottom-element');
-    // div6.style("background-color", "rgba(255,255,255,0.8)");
-    // div6.style("text-align", "center");
-    // div6.style("font-family", "typewr_b");
-    // div6.style("font-size", "7px");
-    // div6.style("display", "flex");
-    // div6.style("justify-content", "center");
-    // div6.style("align-items", "center");
-    // div6.style("border", "1px dotted#000");
 
     //Div7
-    let div7 = createDiv('20231219');
+    let div7 = createDiv('<span>20231219</span>');
     div7.addClass('receipt-bottom-element');
     div7.style("font-family", "barcord");  
     div7.style("font-size", "36px");
-    // div7.style("background-color", "rgba(255,255,255,0.8)");
-    // div7.style("text-align", "center");
-    // div7.style("display", "flex");
-    // div7.style("justify-content", "center");
-    // div7.style("align-items", "center");
-    // div7.style("border", "1px none#000");
+    div7.style("font-stretch","expanded");
 
     //Div8
     let div8 = createDiv('');
     div8.addClass('receipt-bottom-element');
-    // div8.position(
-    //   this.xPos + this.div4Width,
-    //   this.yPos + this.div1Height + this.div2Height + this.div3Height + this.div4Height
-    // );
-    // div8.size(this.div4Width, this.div8Height);
-    // div8.style("background-color", "rgba(255,255,255,0.8)");
-    // div8.style("display", "flex");
-    // div8.style("justify-content", "center");
-    // div8.style("align-items", "center");
-    // div8.style("border", "1px none#000");
+    div8.style("background-image", "url(\"assets/antelope.png\")");
 
-    //let imgElement;
-    //imgElement=createImg('')
 
     //Div9
     let div9 = createDiv('');
     div9.addClass('receipt-bottom-element');
-    // div9.position(
-    //   this.xPos + this.div4Width * 2,
-    //   this.yPos + this.div1Height + this.div2Height + this.div3Height + this.div4Height
-    // );
-    // div9.size(this.div4Width, this.div9Height);
-    // div9.style("background-color", "rgba(255,255,255,0.8)");
-    // div9.style("display", "flex");
-    // div9.style("justify-content", "center");
-    // div9.style("align-items", "center");
-    // div9.style("border", "1px none#000");
-
+    div9.style("background-image", "url(\"assets/qr_dummy.png\")")
 
     receiptBottom.child(div4);
     receiptBottom.child(div5);
